ca87bed506aaee7f679a1a3a3e27a4ad14cf97aa
thangngv95 <thangngv95@gmail.com>
2018-02-03 01:47:51 +0700

    chap-i18n

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,4 @@
 require_relative 'boot'
-
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
@@ -10,6 +9,7 @@ module InitProject
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.i18n.default_locale = :vi
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
