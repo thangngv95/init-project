d62acebc4fee820e7969dc5bc8fc3ccd2f431c07
thangngv95 <thangngv95@gmail.com>
2018-02-27 20:29:18 +0700

    chap9

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,20 +1,32 @@
 class SessionsController < ApplicationController
+  attr_reader :user
+
   def new; end
 
   def create
-    user = User.find_by email: session_params[:email].downcase
-
+    @user = User.find_by email: session_params[:email].downcase
     if user && user.authenticate(session_params[:password])
       log_in user
-      redirect_to user
+      login_success
     else
-      flash.now[:danger] = t ".invalid"
-      render :new
+      login_false
     end
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
+  end
+
+  private
+
+  def login_success
+    session_params[:remember_me] == "1" ? remember(user) : forget(user)
+    redirect_to user
+  end
+
+  def login_false
+    flash.now[:danger] = t ".invalid"
+    render :new
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,18 @@
 class User < ApplicationRecord
+  attr_reader :remember_token
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   validates :name, presence: true, length: {maximum: Settings.name.maximum}
   validates :email, format: {with: VALID_EMAIL_REGEX},
     presence: true, uniqueness: {case_sensitive: false},
     length: {maximum: Settings.email.maximum}
-  validates :password, presence: true,
+  validates :password, presence: true, allow_nil: true,
    length: {minimum: Settings.password.minimum}
 
   before_save :downcase_email
 
   has_secure_password
-
-  private
-
-  def downcase_email
-    email.downcase!
-  end
 
   class << self
     def digest string
@@ -28,5 +24,29 @@ class User < ApplicationRecord
         end
       BCrypt::Password.create string, cost: cost
     end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember
+    @remember_token = User.new_token
+    update_attributes remember_digest: User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    return false unless remember_digest
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
+
+  def forget
+    update_attributes remember_digest: nil
+  end
+
+  private
+
+  def downcase_email
+    email.downcase!
   end
 end
