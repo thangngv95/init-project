f2be17fd071afb25c2567693438d3202b66c159d
thangngv95 <thangngv95@gmail.com>
2018-03-02 22:49:33 +0700

    chap14

---
+++ app/controllers/relationships_controller.rb
@@ -0,0 +1,24 @@
+class RelationshipsController < ApplicationController
+  before_action :logged_in_user
+
+  def create
+    @user = User.find(params[:followed_id])
+    current_user.follow @user
+    follow_user
+  end
+
+  def destroy
+    @user = Relationship.find(params[:id]).followed
+    current_user.unfollow @user
+    follow_user
+  end
+
+  private
+
+  def follow_user
+    respond_to do |format|
+      format.html{redirect_to user}
+      format.js
+    end
+  end
+end
