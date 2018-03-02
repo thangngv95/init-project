14ceb72f596de5785723b251bd3981a7e951e0f9
thangngv95 <thangngv95@gmail.com>
2018-03-02 22:49:33 +0700

    chap 10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,13 @@
 class UsersController < ApplicationController
   attr_reader :user
-  def show
-    @user = User.find_by id: params[:id]
-    return if user
-    flash[:success] = t ".falsed"
-    redirect_to root_path
+
+  before_action :logged_in_user, only: %i(index edit update destroy)
+  before_action :find_user, only: %i(show edit update destroy)
+  before_action :correct_user, only: %i(edit update)
+  before_action :admin_user, only: %i(destroy)
+
+  def index
+    @users = User.paginate page: params[:page]
   end
 
   def new
@@ -22,10 +25,52 @@ class UsersController < ApplicationController
     end
   end
 
+  def show; end
+
+  def edit; end
+
+  def update
+    if user.update_attributes user_params
+      flash[:success] = t ".profile_updated"
+      redirect_to user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    user.destroy
+    flash[:success] = t ".del"
+    redirect_to users_url
+  end
+
   private
 
   def user_params
     params.require(:user).permit :name, :email, :password,
       :password_confirmation
+  end
+
+  def correct_user
+    redirect_to root_url unless user.current_user? current_user
+  end
+
+  def admin_user
+    redirect_to root_url unless current_user.admin?
+  end
+
+  def logged_in_user
+    return if logged_in?
+    store_location
+    flash[:danger] = t ".please"
+    redirect_to login_url
+  end
+
+  def find_user
+    @user = User.find_by id: params[:id]
+
+    return if user
+    flash[:success] = t ".falsed"
+    redirect_to root_path
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -35,6 +35,15 @@ module SessionsHelper
     params[:session]
   end
 
+  def redirect_back_or default
+    redirect_to session[:forwarding_url] || default
+    session.delete :forwarding_url
+  end
+
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
+
   private
 
   def cookies_permanent
